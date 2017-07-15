d7a128808ee5c2a9810131028f6c5383d60687be
Eduardo D'Avila <eduardo@eduardodavila.com.br>
2017-07-15 16:47:49 -0300

    start

--- src/hello/FloatCharDoubleShort.java
+++ src/hello/FloatCharDoubleShort.java
@@ -3,6 +3,17 @@ package hello;
 /**
  * Created by Eduardo on 15/07/2017.
  */
+
 public class FloatCharDoubleShort {
-    
+    public static void main(String[] args) {
+        //Integers
+        int myAge = 45;
+
+        //Byte - smaill type
+        byte myByte = 123;
+
+        System.out.println("My byte is: " + myByte);
+    }
+
+
 }
