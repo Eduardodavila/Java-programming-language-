6ddfa230f696129321b4bfa2ad2437085f77b113
Eduardo D'Avila <eduardo@eduardodavila.com.br>
2017-07-15 16:59:02 -0300

    start

--- src/hello/FloatCharDoubleShort.java
+++ src/hello/FloatCharDoubleShort.java
@@ -10,9 +10,25 @@ public class FloatCharDoubleShort {
         int myAge = 45;
 
         //Byte - smaill type
-        byte myByte = 123;
-
+        byte myByte = 127;
         System.out.println("My byte is: " + myByte);
+
+        //Short - 2x byte
+        short myShort = 128;
+        System.out.println("My Short is: " + myShort);
+
+        //Float
+        float myFloat = 3.14f;
+        System.out.println("My Float is: " + myFloat);
+
+        //Double
+        double myDouble = 23.89;
+        System.out.println("My Double is: " + myDouble);
+
+        //Char == character
+        char myChar = 'E';
+        System.out.println("My Char is: " + myChar);
+
     }
 
 
