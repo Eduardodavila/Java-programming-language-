7809e0b1d053897d25ac479730db976a7a80f944
Eduardo D'Avila <eduardo@eduardodavila.com.br>
2017-07-23 18:54:56 -0300

    Arrays Basic 101

---
+++ src/basicJava/Arrays.java
@@ -0,0 +1,12 @@
+package basicJava;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        int[] myArray = {1, 2, 3, 4};
+        String[] myStringArray = {"Hello", "Hi", "What"};
+
+        System.out.println(myArray[0]);
+        System.out.println(myStringArray[0]);
+    }
+}
