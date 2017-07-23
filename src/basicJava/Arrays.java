7da21914ab2abbd41d0b5e52fd99d8a34cefef89
Eduardo D'Avila <eduardo@eduardodavila.com.br>
2017-07-23 20:32:15 -0300

    Arrays Basic 101

--- src/basicJava/Arrays.java
+++ src/basicJava/Arrays.java
@@ -5,7 +5,8 @@ public class Arrays {
 
         int[] myArray = {1, 2, 3, 4};
         String[] myStringArray = {"Hello", "Hi", "What"};
-        int[] newArray = new int[5];
+        int[] newArray = new int[6];
+        String[] newStringArray = new String[5];
 
         System.out.println(myArray[0]);
         System.out.println(myStringArray[0]);
@@ -17,5 +18,19 @@ public class Arrays {
         for (int j = 0; j < myStringArray.length; j++) {
             System.out.println("Item myStringArray #= " + myStringArray[j]);
         }
+
+        newArray[0] = 3;
+        newArray[1] = 4;
+        newArray[2] = 6;
+        newArray[3] = 1;
+        newArray[4] = 12;
+        newArray[5] = 54;
+
+
+        for (int k = 0; k < newArray.length; k++) {
+            System.out.println("newArray -> " + newArray[k]);
+        }
+
+
     }
 }
