0b6e33430579bd62c6f4107a5159cee7e0340962
Eduardo D'Avila <eduardo@eduardodavila.com.br>
2017-07-23 21:24:46 -0300

    Hashmaps

--- src/basicJava/Hashmaps.java
+++ src/basicJava/Hashmaps.java
@@ -1,9 +1,17 @@
 package basicJava;
 
+import java.util.HashMap;
+
+
 public class Hashmaps {
     public static void main(String[] args) {
         //HashMaps
-        Hashmaps users = new Hashmaps();
+        HashMap users = new HashMap();
+        users.put("Sandra", 23);
+        users.put("paul", 31);
+        users.put("George", 74);
+        System.out.println(users.entrySet());
+
 
 
 
